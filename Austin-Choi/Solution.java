d259a53cb9c1e1eb2eb827ed080cb501062c0fac
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-07-27 17:19:48 +0900

    같은 숫자는 싫어 / 15분

--- Austin-Choi/Solution.java
+++ Austin-Choi/Solution.java
@@ -1,19 +1,27 @@
-package com.edu.algo.폰켓몬.test;
+package com.edu.algo.같은숫자는싫어.test;
+
 import java.util.*;
 
-//10분
-class Solution {
-    public int solution(int[] nums) {
-        Set<Integer> s = new HashSet<>();
-        for(int i : nums)
-        {
-            s.add(i);
+//15분
+public class Solution {
+    public ArrayList<Integer> solution(int []arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        //순서대로 큐에 넣고 중복없이 answer에 저장
+        Queue<Integer> q = new LinkedList<>();
+        for(int i : arr){
+            q.add(i);
         }
-        System.out.println(s);
-        if(s.size() <= nums.length / 2)
-            return s.size();
-        else
-            return nums.length/2;
+        int cur = 0;
+        int prev = q.poll();
+        answer.add(prev);
+        
+        while(!q.isEmpty()){
+            cur = q.poll();
+            if(cur != prev)
+                answer.add(cur);
+            prev = cur;
+        }
+        return answer;
     }
     public static void main(String[] args) {
 		
