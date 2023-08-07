f054ec04b8900e8b16d285b1d24ea17da25d158a
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-08-07 22:05:03 +0900

    PGS_전화번호목록.java / 10분

--- Austin-Choi/PGS_전화번호목록.java
+++ Austin-Choi/PGS_전화번호목록.java
@@ -1,3 +1,4 @@
+//풀이 1 : HashMap 
 import java.util.*;
 
 class Solution {
@@ -34,3 +35,21 @@ class Solution {
         return true;
     }
 }
+/*풀이 2 10분
+import java.util.*;
+
+class Solution {
+    //풀이 2
+    //Str1.startsWith(str2) 이용
+    public boolean solution(String[] pb) {
+        Arrays.sort(pb);
+        //정렬 안하면 버블소팅처럼 O(n^2)복잡도인데
+        //정렬하면 바로 뒤에꺼만 비교해주면 됨 O(n)
+        for(int i = 0; i<pb.length-1; i++){
+            if(pb[i+1].startsWith(pb[i]))
+                return false;
+        }
+        return true;
+    }
+}
+*/
