1f6095e767a0cd19ae247746c001c9bf5cb5be0c
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-10-26 14:44:24 +0900

    BOJ_19583_싸이버개강총회 / 13분

---
+++ Austin-Choi/BOJ_19583_싸이버개강총회.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_19583_싸이버개강총회 {
+	public static int timeToInt(String time) {
+		StringBuilder sb = new StringBuilder();
+		String[] arr = time.split("");
+		for(int i = 0; i<5; i++) {
+			if(i == 2)
+				continue;
+			else
+				sb.append(arr[i]);
+		}
+		return Integer.parseInt(sb.toString());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		Set<String> start = new HashSet<String>();
+		Set<String> end = new HashSet<String>();
+		// 0 <= start <= S, E <= end <= Q
+		int S, E, Q;
+		S = timeToInt(st.nextToken());
+		E = timeToInt(st.nextToken());
+		Q = timeToInt(st.nextToken());
+		
+		int curTime = 0;
+		String curName;
+		while(true) {
+			try {
+				st = new StringTokenizer(br.readLine());
+				curTime = timeToInt(st.nextToken());
+				curName = st.nextToken();
+			}
+			catch (IOException e){
+				break;
+			}
+			catch (NoSuchElementException e) {
+				break;
+			}	
+			
+			if(curTime <= S && curTime >= 0)
+				start.add(curName);
+			if(curTime <= Q && curTime >= E)
+				end.add(curName);
+		}
+		int total = 0;
+		for(String s : end) {
+			if(start.contains(s))
+				total++;
+		}
+		System.out.println(total);
+	}
+}
